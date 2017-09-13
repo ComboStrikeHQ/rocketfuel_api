52d777b45943a3ef9827d373f09248c3e1c4ea95
Stefan Greffenius <stefan.greffenius@ad2games.com>
2017-09-13 16:55:40 +0200

    Memoization for YAML.load_file

--- lib/rocketfuel_api/service.rb
+++ lib/rocketfuel_api/service.rb
@@ -20,9 +20,11 @@ class RocketfuelApi::Service
   end
 
   def uri_suffix
-    file_path = RocketfuelApi.root.join('lib', 'config', 'endpoints_for_services.yaml')
-    endpoints = YAML.load_file(file_path)
-    endpoint  = endpoints['service'][name]
+    @endpoints ||= YAML.load_file(
+      RocketfuelApi.root.join('lib', 'config', 'endpoints_for_services.yaml')
+    )
+
+    endpoint  = @endpoints['service'][name]
 
     endpoint || raise(RocketfuelApi::NotImplemented,
       format('No endpoint for service %s available.', name))
